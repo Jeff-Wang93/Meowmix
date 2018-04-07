ccf66aedd0e904e0a31a089d1a524b639d8fdc16
Jeff Wang <jeffwang93@gmail.com>
2018-04-06 17:39:27 -0700

    Added permissions to the manifest. Added documentation to Song class.

--- app/src/main/java/com/example/jeff/meowmix/Song.java
+++ app/src/main/java/com/example/jeff/meowmix/Song.java
@@ -1,25 +1,48 @@
 package com.example.jeff.meowmix;
 
+/** Basically the metadata of a song
+ *
+ */
 public class Song {
     private String album;
     private String artist;
-    private String genre;
     private long id;
     private String title;
 
-    public Song(String songAlbum, String songArtist, String songGenre, long songId, String
+
+    /** Constructor for a Song object
+     * @param songAlbum The song's album name
+     * @param songArtist The song's artist/artists name
+     * @param songId The song's Id
+     * @param songTitle The song's title
+     */
+    public Song(String songAlbum, String songArtist, long songId, String
             songTitle) {
         album = songAlbum;
         artist = songArtist;
-        genre = songGenre;
         id = songId;
         title = songTitle;
     }
 
-    // getter methods
+    /** Get the song's album name
+     * @return A string containing the song's album name
+     */
     public String getAlbum() { return album; }
+
+    /** Get the song's artist name
+     * @return A string containing the song's artist's name
+     */
     public String getArtist() { return artist; }
-    public String getGenre() { return genre; }
+
+
+    /** Get the song's Id
+     * @return A long representing the song's Id
+     */
     public long getId() { return id; }
+
+
+    /** Get the song's title
+     * @return A string containing the song's title
+     */
     public String getTitle() { return title; }
 }
